26c8b75e35f18b267042fa8e8c9e534d34e30de8
hugosenari <hugosenari@gmail.com>
2022-03-27 22:47:24 -0300

    chore: re index github-disruptek-gittyup

--- 1_0_0/flake.nix
+++ 1_0_0/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''higher-level libgit2 bindings'';
+  description = ''comfort for nimgit2'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,22 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-github-disruptek-gittyup-1_0_0.flake = false;
-  inputs.src-github-disruptek-gittyup-1_0_0.ref   = "refs/tags/1.0.0";
-  inputs.src-github-disruptek-gittyup-1_0_0.owner = "disruptek";
-  inputs.src-github-disruptek-gittyup-1_0_0.repo  = "gittyup";
-  inputs.src-github-disruptek-gittyup-1_0_0.type  = "github";
+  inputs.src-gittyup-1_0_0.flake = false;
+  inputs.src-gittyup-1_0_0.ref   = "refs/tags/1.0.0";
+  inputs.src-gittyup-1_0_0.owner = "disruptek";
+  inputs.src-gittyup-1_0_0.repo  = "gittyup";
+  inputs.src-gittyup-1_0_0.type  = "github";
+  
+  inputs."nimgit2".owner = "nim-nix-pkgs";
+  inputs."nimgit2".ref   = "master";
+  inputs."nimgit2".repo  = "nimgit2";
+  inputs."nimgit2".dir   = "v0_3_4";
+  inputs."nimgit2".type  = "github";
+  inputs."nimgit2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgit2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-github-disruptek-gittyup-1_0_0"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-gittyup-1_0_0"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-github-disruptek-gittyup-1_0_0";
+    src  = deps."src-gittyup-1_0_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
--- 2_0_1/flake.nix
+++ 2_0_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''higher-level libgit2 bindings'';
+  description = ''higher-level git bindings that build upon nimgit2'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,22 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-github-disruptek-gittyup-2_0_1.flake = false;
-  inputs.src-github-disruptek-gittyup-2_0_1.ref   = "refs/tags/2.0.1";
-  inputs.src-github-disruptek-gittyup-2_0_1.owner = "disruptek";
-  inputs.src-github-disruptek-gittyup-2_0_1.repo  = "gittyup";
-  inputs.src-github-disruptek-gittyup-2_0_1.type  = "github";
+  inputs.src-gittyup-2_0_1.flake = false;
+  inputs.src-gittyup-2_0_1.ref   = "refs/tags/2.0.1";
+  inputs.src-gittyup-2_0_1.owner = "disruptek";
+  inputs.src-gittyup-2_0_1.repo  = "gittyup";
+  inputs.src-gittyup-2_0_1.type  = "github";
+  
+  inputs."nimgit2".owner = "nim-nix-pkgs";
+  inputs."nimgit2".ref   = "master";
+  inputs."nimgit2".repo  = "nimgit2";
+  inputs."nimgit2".dir   = "v0_1_1";
+  inputs."nimgit2".type  = "github";
+  inputs."nimgit2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgit2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."result".owner = "nim-nix-pkgs";
+  inputs."result".ref   = "master";
+  inputs."result".repo  = "result";
+  inputs."result".dir   = "master";
+  inputs."result".type  = "github";
+  inputs."result".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."result".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-github-disruptek-gittyup-2_0_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-gittyup-2_0_1"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-github-disruptek-gittyup-2_0_1";
+    src  = deps."src-gittyup-2_0_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
